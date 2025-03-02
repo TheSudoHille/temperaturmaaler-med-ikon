b3ab9fdf89ff207d3d425aba2638a3dfa84a8929
TheSudoHille <h.theander@icloud.com>
2025-03-02 10:22:08 +0100

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ basic.forever(function () {
     temperatur = input.temperature() - 2
     basic.showNumber(temperatur)
     basic.pause(3000)
-    if (temperatur > 21) {
+    if (temperatur < 21 || temperatur < 24) {
     	
     }
     basic.showIcon(IconNames.Happy)
